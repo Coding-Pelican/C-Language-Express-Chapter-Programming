4dcb08f996170d44258c37a1a68964c751703ca2
Gyeongtae Kim <codingpelican@gmail.com>
2022-01-24 16:58:21 +0900

    Update programming_code.c

--- Chapter9/programming_code.c
+++ Chapter9/programming_code.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
-#define PI 3.141592
 #define TRUE 1
 #define FALSE 0
 
@@ -64,17 +63,14 @@ double div(double _x, double _y){
 	n_div++;
 	return _x / _y;
 }
-
 double mul(double _x, double _y){
 	n_mul++;
 	return _x * _y;
 }
-
 double sub(double _x, double _y){
 	n_sub++;
 	return _x - _y;
 }
-
 double add(double _x, double _y){
 	n_add++;
 	return _x + _y;
@@ -108,5 +104,208 @@ void count_dice_face(int face){
 			face_cnt[i] = 0;
 		}
 	}
+}
+*/
+//03
+/*
+int check(int value_of_input);
+int input_password();
+const int max_number_of_verify = 3;
+const int password = 1234;
+int main(void){
+	while(TRUE){
+		if(check(input_password()) == FALSE){
+			continue;
+		} else{
+			break;
+		}
+	}
+}
+int input_password(){
+	static int value_of_input = 1000; //int value_of_input;
+	printf("Password : ");
+	printf("%d\n", value_of_input++); //scanf("%d", &value_of_input);
+	return value_of_input;
+}
+int check(int value_of_input){
+	static int number_of_verify = 0;
+	number_of_verify++;
+	if(number_of_verify < max_number_of_verify){
+		return value_of_input == password ? TRUE : FALSE;
+	} else{
+		puts("Login Attempts Exceeded");
+		return TRUE;
+	}
+}
+*/
+//04
+/*
+void get_random();
+int main(void){
+	get_random();
+	get_random();
+	get_random();
+	get_random();
+}
+void get_random(){
+	static int isInited = FALSE;
+	if(isInited == FALSE){
+		isInited = TRUE;
+		srand((unsigned)time(NULL));
+		puts("Procced initialization");
+	} else{
+		printf("%d\n", rand());
+	}  
+}
+*/
+//05
+/*
+int sum(int n);
+int get_integer();
+int main(void){
+	printf("%d\n", sum(get_integer()));
+}
+int get_integer(){
+	int n;
+	printf("Enter an Integer : ");
+	printf("%d\n", n = 10); //scanf("%d", &n);
+	return n;
+}
+int sum(int n){
+	return n == 1 ? 1 : n + sum(n - 1);
+}
+*/
+//06
+/*
+int power(int base, int power_raised);
+int get_integer(int value);
+int main(void){
+	int base, exponent;
+	printf("Enter an base : ");
+	base = get_integer(2);
+	printf("Enter an exponent : ");
+	exponent = get_integer(10);
+	printf("%d\n", power(base, exponent));
+}
+int get_integer(int value){ //parameter is void
+	int n;
+	printf("%d\n", n = value); //scanf("%d", &n);
+	return n;
+}
+int power(int base, int power_raised){
+	return power_raised == 1 ? base : base * power(base, power_raised - 1);
+}
+*/
+//07
+/*
+int get_integer(int value);
+int show_digit(int x);
+int main(void){
+	show_digit(get_integer(1234));
+}
+int show_digit(int x){
+	if(x > 0){
+		show_digit(x / 10);
+		printf("%d ", x % 10);
+	}
+}
+int get_integer(int value){ //parameter is void
+	int n;
+	printf("%d\n", n = value); //scanf("%d", &n);
+	return n;
+}
+*/
+//08
+/*
+int get_integer(int value);
+int count_number_of_digit(int n);
+int main(void){
+	printf("%d\n", count_number_of_digit(get_integer(54321)));
+}
+int count_number_of_digit(int n){
+	static int cnt = 0;
+	if(n > 0){
+		count_number_of_digit(n / 10);
+		cnt++;
+	}
+	return cnt;
+}
+int get_integer(int value){ //parameter is void
+	int n;
+	printf("%d\n", n = value); //scanf("%d", &n);
+	return n;
+}
+*/
+//09
+/*
+int get_integer(int value);
+int sum_digits(int x);
+int main(void){
+	printf("%d\n", sum_digits(get_integer(123)));
+}
+int sum_digits(int x){
+	static int sum = 0;
+	if(x > 0){
+		sum_digits(x / 10);
+		sum += x % 10;
+	}
+	return sum;
+}
+int get_integer(int value){ //parameter is void
+	int n;
+	printf("%d\n", n = value); //scanf("%d", &n);
+	return n;
+}*/
+//10
+/*
+double get_double(int value);
+double recursive(double n);
+int main(void){
+	printf("%lf\n", recursive(get_double(10)));
+}
+double recursive(double n){
+	return n == 1 ? n : 1.0 / n + recursive(n - 1)s;
+}
+double get_double(int value){ //parameter is void
+	double n;
+	printf("%lf\n", n = value); //scanf("%d", &n);
+	return n;
+}
+*/
+//11
+/*
+int get_integer(int value);
+int recursive(int n, int k);
+int main(void){
+	int n, k;
+	printf("n = ");
+	n = get_integer(3);
+	printf("k = ");
+	k = get_integer(2);
+	printf("%d\n", recursive(n, k));
+}
+int recursive(int n, int k){
+	return k == 0 || k == n ? 1 : recursive(n - 1, k - 1) + recursive(n - 1, k);
+}
+int get_integer(int value){ //parameter is void
+	int n;
+	printf("%d\n", n = value); //scanf("%d", &n);
+	return n;
+}
+*/
+//12
+/*
+int get_integer(int value);
+int fib(int n);
+int main(void){
+	printf("%d\n", fib(get_integer(9)));
+}
+int fib(int n){
+	return n == 0 ? 0 : n == 1 ? 1 : fib(n - 2) + fib(n - 1);
+}
+int get_integer(int value){ //parameter is void
+	int n;
+	printf("%d\n", n = value); //scanf("%d", &n);
+	return n;
 }
 */
