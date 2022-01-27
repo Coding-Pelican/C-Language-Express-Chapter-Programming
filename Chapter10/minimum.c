14b64edc3d1dc4dd7207a2e073d5d5d214910ae5
Gyeongtae Kim <codingpelican@gmail.com>
2022-01-27 17:36:41 +0900

    Update minimum.c

--- Chapter10/minimum.c
+++ Chapter10/minimum.c
@@ -6,7 +6,7 @@
 void print_horizontal_line(int n);
 int min_comparison(int n, int m);
 
-int main(void){
+int main(void) {
 	srand((unsigned)time(NULL));
 	int prices[SIZE] = { 0, };
 	int min;
@@ -17,7 +17,7 @@ int main(void){
 	}
 	putchar('\n');
 	for (int i = 0; i < SIZE; i++) {
-		prices[i] = rand()%100 + 1;
+		prices[i] = rand() % 100 + 1;
 		printf("%3d ", prices[i]);
 	}
 	putchar('\n');
@@ -32,7 +32,7 @@ int main(void){
 
 int min_comparison(int n, int m) { return n < m ? n : m; }
 void print_horizontal_line(int n){ 
-	for(int i = 0; i < n * 4; i++){
+	for(int i = 0; i < n * 4; i++) {
 		printf("-");
 	}
 	putchar('\n');
