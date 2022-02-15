cbbd8a1a8d46dd0fe3c15ed83627f728c12234ff
Gyeongtae Kim <codingpelican@gmail.com>
2022-02-15 17:07:37 +0900

    Create swap2.c

---
+++ Chapter11/swap2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void swap(int* x, int* y);
+
+int main(void) {
+	int a = 100, b = 200;
+	
+	printf("a=%d b=%d\n", a, b);
+	swap(&a, &b);
+	printf("a=%d b=%d\n", a, b);
+}
+
+void swap(int* x, int* y) {
+	int temp;
+	temp = *x;
+	*x = *y; 
+	*y = temp;
+}
