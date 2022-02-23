777cc4bc266e42bd49a1f3d5884d11713d012bae
Gyeongtae Kim <codingpelican@gmail.com>
2022-02-23 17:17:59 +0900

    Update self_check_5.c

--- JUNGOL/Language_Coder/Function_3/self_check_5.c
+++ JUNGOL/Language_Coder/Function_3/self_check_5.c
@@ -1,11 +1,24 @@
 #include <bits/stdc++.h>
+using namespace std;
 int n;
-int* combination;
-int* level;
-void search(){
-	
+int* arr;
+void search(int depth) {
+	if (depth > n) {
+		for (int i = 1; i < n + 1; i++) {
+			cout << arr[i];
+		}
+		cout << "\n";
+	} else {
+		for (int i = arr[depth - 1]; i < 7; i++) {
+			arr[depth] = i;
+			search(depth + 1);
+		}
+	}
 }
 int main(void) {
-	scanf("%d", &n);
-	search();
+	cin >> n;
+	arr = (int*)calloc(sizeof(int), n);
+	arr[0] = 1;
+	search(1);
+	free(arr);
 }
