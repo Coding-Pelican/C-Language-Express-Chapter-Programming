7e5072a8c077ca98a24b51814fcabb7da042676b
Gyeongtae Kim <codingpelican@gmail.com>
2022-02-24 17:00:39 +0900

    Create stringconst.c

---
+++ Chapter12/stringconst.c
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+int main(void) {
+	char* p = "HelloWorld";
+	printf("%s \n", p);
+	p = "Welcome to C World!";
+	printf("%s \n", p);
+	p = "Goodbye";
+	printf("%s \n", p);
+}
