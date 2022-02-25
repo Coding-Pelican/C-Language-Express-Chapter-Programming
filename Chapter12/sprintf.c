392bc483943f9325546c7d669682fa7f55f4dce6
Gyeongtae Kim <codingpelican@gmail.com>
2022-02-25 17:23:44 +0900

    Create sprintf.c

---
+++ Chapter12/sprintf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+int main(void) {
+	char filename[100];
+	char s[100];
+	for (int i = 0; i < 6; i++) {
+		strcpy(filename, "image");
+		sprintf(s, "%d", i);
+		strcat(filename, s);
+		strcat(filename, ".jpg");
+		printf("%s \n", filename);
+	}
+}
